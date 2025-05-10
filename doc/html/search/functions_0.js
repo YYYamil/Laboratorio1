7be17c1eb71477b247780ca890494f186d8d9c09
Tolabay <yamiltolaba@gmail.com>
2025-05-10 11:55:52 -0300

    Serializando estructuras y documentos doxygen

---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['serializar_0',['serializar',['../alumno_8c.html#a8ac6bdc615decee37579a12f51110bfe',1,'Serializar(const alumno_t alumno, char buffer[], uint32_t size):&#160;alumno.c'],['../alumno_8h.html#a4ebc3a540585705b67cc4bf074945a8c',1,'Serializar(alumno_t alumno, char buffer[], uint32_t disponibles):&#160;alumno.c']]]
+];
---
+++ doc/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nombre_0',['nombre',['../structalumno__s.html#a2235c14796c52614b493b1963c71a26d',1,'alumno_s']]]
+];
